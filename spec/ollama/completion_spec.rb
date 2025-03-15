eca71d953b66f72746125794bb9c537a8085df27
0x1eef <0x1eef@protonmail.com>
2025-03-15 20:02:17 -0300

    spec: add completion fixtures for Ollama

--- spec/ollama/completion_spec.rb
+++ spec/ollama/completion_spec.rb
@@ -2,7 +2,7 @@
 
 require "webmock/rspec"
 
-RSpec.describe "LLM::Ollama" do
+RSpec.describe "LLM::Ollama: completions" do
   subject(:ollama) { LLM.ollama("") }
 
   before(:each, :success) do
@@ -10,115 +10,43 @@ RSpec.describe "LLM::Ollama" do
       .with(headers: {"Content-Type" => "application/json"})
       .to_return(
         status: 200,
-        body: '{
-          "model": "llama3.2",
-          "created_at": "2023-12-12T14:13:43.416799Z",
-          "message": {
-            "role": "assistant",
-            "content": "Hello! How are you today?"
-          },
-          "done": true,
-          "total_duration": 5191566416,
-          "load_duration": 2154458,
-          "prompt_eval_count": 26,
-          "prompt_eval_duration": 383809000,
-          "eval_count": 298,
-          "eval_duration": 4799921000
-        }',
+        body: fixture("ollama/completions/ok_completion.json"),
         headers: {"Content-Type" => "application/json"}
       )
   end
 
-  # before(:each, :unauthorized) do
-  #   stub_request(:post, "https://api.openai.com/v1/chat/completions")
-  #     .with(headers: {"Content-Type" => "application/json"})
-  #     .to_return(
-  #       status: 401,
-  #       body: '{
-  #         "error": {
-  #           "message": "Incorrect API key provided: sk-6WQZM***************************************t6ea. You can find your API key a t https://platform.openai.com/account/api-keys.",
-  #           "type": "invalid_request_error",
-  #           "param": null,
-  #           "code": "invalid_api_key"
-  #         }
-  #         }',
-  #       headers: {"Content-Type" => "application/json"}
-  #     )
-  # end
-
-  # before(:each, :bad_request) do
-  #   stub_request(:post, "https://api.openai.com/v1/chat/completions")
-  #     .with(headers: {"Content-Type" => "application/json"})
-  #     .to_return(
-  #       status: 400,
-  #       body: {
-  #         error: {
-  #           message: "Failed to download image from /path/to/nowhere.bin. Image URL is invalid.",
-  #           type: "invalid_request_error",
-  #           param: nil,
-  #           code: "invalid_image_url"
-  #         }
-  #       }.to_json
-  #     )
-  # end
-
-  context "with successful completion", :success do
-    let(:completion) { ollama.complete("Hello!", :user) }
-    let(:choice) { completion.choices[0] }
+  context "when given a successful response", :success do
+    subject(:response) { ollama.complete("Hello!", :user) }
 
     it "returns a completion" do
-      expect(completion).to be_a(LLM::Response::Completion)
+      expect(response).to be_a(LLM::Response::Completion)
     end
 
-    it "has model" do
-      expect(completion.model).to eq("llama3.2")
+    it "returns a model" do
+      expect(response.model).to eq("llama3.2")
     end
 
-    it "has choices" do
-      expect(completion.choices.first).to have_attributes(
-        role: "assistant",
-        content: "Hello! How are you today?"
-      )
-    end
-
-    it "has token usage" do
-      expect(completion).to have_attributes(
+    it "includes token usage" do
+      expect(response).to have_attributes(
         prompt_tokens: 26,
         completion_tokens: 298,
         total_tokens: 324
       )
     end
 
-    it "stores the completion as context" do
-      expect(choice.extra[:completion]).to eq(completion)
+    context "with a choice" do
+      subject(:choice) { response.choices[0] }
+
+      it "has choices" do
+        expect(choice).to have_attributes(
+          role: "assistant",
+          content: "Hello! How are you today?"
+        )
+      end
+
+      it "includes the response" do
+        expect(choice.extra[:completion]).to eq(response)
+      end
     end
   end
-
-  # context "with an unauthorized error", :unauthorized do
-  #   let(:completion) { openai.complete(LLM::Message.new("user", "Hello!")) }
-
-  #   it "raises an error" do
-  #     expect { completion }.to raise_error(LLM::Error::Unauthorized)
-  #   end
-
-  #   it "includes a response" do
-  #     completion
-  #   rescue LLM::Error::Unauthorized => ex
-  #     expect(ex.response).to be_kind_of(Net::HTTPResponse)
-  #   end
-  # end
-
-  # context "with a bad request", :bad_request do
-  #   subject(:completion) { openai.complete(LLM::Message.new("user", URI("/path/to/nowhere.bin"))) }
-
-  #   it "raises an error" do
-  #     expect { completion }.to raise_error(LLM::Error::BadResponse)
-  #   end
-
-  #   it "includees a response" do
-  #     completion
-  #   rescue LLM::Error => ex
-  #     expect(ex.response).to be_instance_of(Net::HTTPBadRequest)
-  #   end
-  # end
 end
