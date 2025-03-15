e10ca71b81337c43095b02486cf094b9b278d702
0x1eef <0x1eef@protonmail.com>
2025-03-15 08:21:41 -0300

    Fix `LLM::Response::Embedding#parsed`
    This method would raise a `NoMethodError` by calling `@provider.parse_embedding`
    instead of calling `parse_embedding` with `Response::Embedding` as self.
    
    Also add tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,14 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include Module.new {
+    def fixture(file)
+      File.read File.join(fixtures, file)
+    end
+
+    def fixtures
+      File.join(__dir__, "fixtures", "responses")
+    end
+  }
 end
