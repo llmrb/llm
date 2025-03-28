73a8ae2be3941c96d4bb483f355e2c87160f40b1
0x1eef <0x1eef@protonmail.com>
2025-03-28 02:00:44 +0000

    specs: remove unused fixtures

--- spec/setup.rb
+++ spec/setup.rb
@@ -10,23 +10,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
-
-  config.include Module.new {
-    def request_fixture(file)
-      path = File.join(fixtures, "requests", file)
-      File.read(path).chomp
-    end
-
-    def response_fixture(file)
-      path = File.join(fixtures, "responses", file)
-      File.read(path).chomp
-    end
-    alias_method :fixture, :response_fixture
-
-    def fixtures
-      File.join(__dir__, "fixtures")
-    end
-  }
 end
 
 VCR.configure do |config|
