bd9d0b1d66db8d18d4d970f63f07e7bb7662b948
0x1eef <0x1eef@protonmail.com>
2025-04-20 03:13:34 +0000

    gemini: add specs

---
+++ spec/gemini/images_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "setup"
+
+RSpec.describe "LLM::OpenAI::Images" do
+  let(:token) { ENV["LLM_SECRET"] || "TOKEN" }
+  let(:provider) { LLM.gemini(token) }
+
+  context "when given a successful creation",
+        vcr: {cassette_name: "gemini/images/successful_creation"} do
+    subject(:response) { provider.images.create(prompt: "A dog on a rocket to the moon") }
+
+    it "is successful" do
+      expect(response).to be_instance_of(LLM::Response::Image)
+    end
+
+    it "returns an encoded string" do
+      expect(response.image.encoded).to be_instance_of(String)
+    end
+
+    it "returns a binary string" do
+      expect(response.image.binary).to be_instance_of(String)
+    end
+  end
+
+  context "when given a successful edit",
+        vcr: {cassette_name: "gemini/images/successful_edit"} do
+    subject(:response) do
+      provider.images.edit(
+        image: LLM::File("spec/fixtures/images/bluebook.png"),
+        prompt: "Book is floating in the clouds"
+      )
+    end
+
+    it "is successful" do
+      expect(response).to be_instance_of(LLM::Response::Image)
+    end
+
+    it "returns data" do
+      expect(response.image.encoded).to be_instance_of(String)
+    end
+
+    it "returns a url" do
+      expect(response.image.binary).to be_instance_of(String)
+    end
+  end
+end
