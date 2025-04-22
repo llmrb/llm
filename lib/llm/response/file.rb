b3d262a596761bed50072ae95aa08f49b22fd133
0x1eef <0x1eef@protonmail.com>
2025-04-22 14:07:33 +0000

    gemini: transform keys instead

--- lib/llm/response/file.rb
+++ lib/llm/response/file.rb
@@ -6,26 +6,37 @@ module LLM
   # that has been uploaded to a provider. Its properties are delegated
   # to the underlying response body, and vary by provider.
   class Response::File < Response
+    ##
+    # Returns a normalized response body
+    # @return [Hash]
+    def body
+      @_body ||= if super["file"]
+        super["file"].transform_keys { snakecase(_1) }
+      else
+        super.transform_keys { snakecase(_1) }
+      end
+    end
+
+    ##
+    # @return [String]
+    def inspect
+      "#<#{self.class}:0x#{object_id.to_s(16)} body=#{body}>"
+    end
+
     private
 
     include LLM::Utils
 
     def respond_to_missing?(m, _)
-      body.key?(m.to_s) || body.key?(camelcase(m)) || super
+      body.key?(m.to_s) || super
     end
 
     def method_missing(m, *args, &block)
       if body.key?(m.to_s)
         body[m.to_s]
-      elsif body.key?(camelcase(m))
-        body[camelcase(m)]
       else
         super
       end
-    end
-
-    def body
-      super["file"] || super
     end
   end
 end
