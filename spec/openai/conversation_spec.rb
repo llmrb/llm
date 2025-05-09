753f67f93ee1d632bca7a4fca13126973a8cbebc
0x1eef <0x1eef@protonmail.com>
2025-05-09 05:48:44 +0000

    Add class-based function support

--- spec/openai/conversation_spec.rb
+++ spec/openai/conversation_spec.rb
@@ -6,49 +6,87 @@ RSpec.describe "LLM::Chat: openai" do
   let(:described_class) { LLM::Chat }
   let(:provider) { LLM.openai(key:) }
   let(:key) { ENV["OPENAI_SECRET"] || "TOKEN" }
-  let(:bot) { described_class.new(provider, **params).lazy }
+  let(:bot) { described_class.new(provider, params).lazy }
 
-  let(:tool) do
-    LLM.function(:system) do |fn|
-      fn.description "Runs system commands, emits their output"
-      fn.params do |schema|
-        schema.object(command: schema.string.required)
+  context "when given a block-based function" do
+    let(:tool) do
+      LLM.function(:system) do |fn|
+        fn.description "Runs system commands, emits their output"
+        fn.params do |schema|
+          schema.object(command: schema.string.required)
+        end
+        fn.define do |params|
+          Kernel.system(params.command)
+        end
       end
-      fn.define do |params|
-        Kernel.system(params.command)
+    end
+
+    context "when given an instance of LLM::Function::Return",
+            vcr: {cassette_name: "openai/conversations/system_function_call_1"} do
+      let(:params) { {tools: [tool]} }
+
+      before do
+        bot.chat("You are a bot that can run UNIX system commands", role: :user)
+        bot.chat("Hey, tell me the date", role: :user)
+      end
+
+      it "calls the function" do
+        expect(Kernel).to receive(:system).with("date").and_return(true)
+        bot.chat bot.functions[0].call
+        expect(bot.functions).to be_empty
+      end
+    end
+
+    context "when given an instance of LLM::Function::Return (via an array)",
+            vcr: {cassette_name: "openai/conversations/system_function_call_2"} do
+      let(:params) { {tools: [tool]} }
+
+      before do
+        bot.chat("You are a bot that can run UNIX system commands", role: :user)
+        bot.chat("Hey, tell me the date", role: :user)
+      end
+
+      it "calls the function" do
+        expect(Kernel).to receive(:system).with("date").and_return(true)
+        bot.chat bot.functions.map(&:call)
+        expect(bot.functions).to be_empty
       end
     end
   end
 
-  context "when given an instance of LLM::Function::Return",
-          vcr: {cassette_name: "openai/conversations/system_function_call_1"} do
-    let(:params) { {tools: [tool]} }
-
-    before do
-      bot.chat("You are a bot that can run UNIX system commands", role: :user)
-      bot.chat("Hey, tell me the date", role: :user)
+  context "when given a class-based function" do
+    let(:klass) do
+      Class.new do
+        def call(params)
+          Kernel.system(params.command)
+        end
+      end
     end
 
-    it "calls the function" do
-      expect(Kernel).to receive(:system).with("date").and_return(true)
-      bot.chat bot.functions[0].call
-      expect(bot.functions).to be_empty
-    end
-  end
-
-  context "when given an instance of LLM::Function::Return (via an array)",
-          vcr: {cassette_name: "openai/conversations/system_function_call_2"} do
-    let(:params) { {tools: [tool]} }
-
-    before do
-      bot.chat("You are a bot that can run UNIX system commands", role: :user)
-      bot.chat("Hey, tell me the date", role: :user)
+    let(:tool) do
+      LLM.function(:system) do |fn|
+        fn.description "Runs system commands, emits their output"
+        fn.params do |schema|
+          schema.object(command: schema.string.required)
+        end
+        fn.register(klass)
+      end
     end
 
-    it "calls the function" do
-      expect(Kernel).to receive(:system).with("date").and_return(true)
-      bot.chat bot.functions.map(&:call)
-      expect(bot.functions).to be_empty
+    context "when given an instance of LLM::Function::Return",
+            vcr: {cassette_name: "openai/conversations/system_function_call_3"} do
+      let(:params) { {tools: [tool]} }
+
+      before do
+        bot.chat("You are a bot that can run UNIX system commands", role: :user)
+        bot.chat("Hey, tell me the date", role: :user)
+      end
+
+      it "calls the function" do
+        expect(Kernel).to receive(:system).with("date").and_return(true)
+        bot.chat bot.functions[0].call
+        expect(bot.functions).to be_empty
+      end
     end
   end
 end
