e0c633dc42caf1883ca6d20374dda7c2c3f9101b
0x1eef <0x1eef@protonmail.com>
2025-05-09 13:16:58 +0000

    multipart: refactor LLM::Multipart

--- lib/llm/multipart.rb
+++ lib/llm/multipart.rb
@@ -28,20 +28,6 @@ class LLM::Multipart
   end
 
   ##
-  # Returns the multipart request body parts
-  # @return [Array<String>]
-  def parts
-    params.map do |key, value|
-      locals = {key: key.to_s.b, boundary: boundary.to_s.b}
-      if value.respond_to?(:path)
-        file_part(key, value, locals)
-      else
-        data_part(key, value, locals)
-      end
-    end
-  end
-
-  ##
   # Returns the multipart request body
   # @return [String]
   def body
@@ -54,47 +40,61 @@ class LLM::Multipart
 
   attr_reader :params
 
+  def file(key, file, locals)
+    locals = locals.merge(attributes(file))
+    build_file(locals:) do |body|
+      IO.copy_stream(file.path, body)
+      body << "\r\n"
+    end
+  end
+
+  def form(key, value, locals)
+    locals = locals.merge(value:)
+    build_form(locals:) do |body|
+      body << value.to_s
+      body << "\r\n"
+    end
+  end
+
+  def build_file(locals)
+    str = StringIO.new("".b)
+    str << "--#{locals[:boundary]}" \
+           "\r\n" \
+           "Content-Disposition: form-data; name=\"#{locals[:key]}\";" \
+           "filename=\"#{locals[:filename]}\"" \
+           "\r\n" \
+           "Content-Type: #{locals[:content_type]}" \
+           "\r\n\r\n"
+    yield(str)
+  end
+
+  def build_form(locals)
+    str = StringIO.new("".b)
+    str << "--#{locals[:boundary]}" \
+           "\r\n" \
+           "Content-Disposition: form-data; name=\"#{locals[:key]}\"" \
+           "\r\n\r\n"
+    yield(str)
+  end
+
+  ##
+  # Returns the multipart request body parts
+  # @return [Array<String>]
+  def parts
+    params.map do |key, value|
+      locals = {key: key.to_s.b, boundary: boundary.to_s.b}
+      if value.respond_to?(:path)
+        file(key, value, locals)
+      else
+        form(key, value, locals)
+      end
+    end
+  end
+
   def attributes(file)
     {
       filename: File.basename(file.path).b,
       content_type: LLM::Mime[file].b
     }
-  end
-
-  def multipart_header(type:, locals:)
-    if type == :file
-      str = StringIO.new("".b)
-      str << "--#{locals[:boundary]}" \
-             "\r\n" \
-             "Content-Disposition: form-data; name=\"#{locals[:key]}\";" \
-             "filename=\"#{locals[:filename]}\"" \
-             "\r\n" \
-             "Content-Type: #{locals[:content_type]}" \
-             "\r\n\r\n"
-    elsif type == :data
-      str = StringIO.new("".b)
-      str << "--#{locals[:boundary]}" \
-             "\r\n" \
-             "Content-Disposition: form-data; name=\"#{locals[:key]}\"" \
-             "\r\n\r\n"
-    else
-      raise "unknown type: #{type}"
-    end
-  end
-
-  def file_part(key, file, locals)
-    locals = locals.merge(attributes(file))
-    multipart_header(type: :file, locals:).tap do |io|
-      IO.copy_stream(file.path, io)
-      io << "\r\n"
-    end
-  end
-
-  def data_part(key, value, locals)
-    locals = locals.merge(value:)
-    multipart_header(type: :data, locals:).tap do |io|
-      io << value.to_s
-      io << "\r\n"
-    end
   end
 end
