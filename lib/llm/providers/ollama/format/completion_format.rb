1f652b05efcfc9793135f8baefbf033a20d8f8e8
0x1eef <0x1eef@protonmail.com>
2025-05-16 07:16:02 +0000

    ollama: fix bug where an array input was not handled correctly

--- lib/llm/providers/ollama/format/completion_format.rb
+++ lib/llm/providers/ollama/format/completion_format.rb
@@ -63,7 +63,7 @@ module LLM::Ollama::Format
       elsif returns.any?
         returns.map { {role: "tool", tool_call_id: _1.id, content: JSON.dump(_1.value)} }
       else
-        [{role: message.role, content: content.flat_map { format_content(_1) }}]
+        content.flat_map { {role: message.role }.merge(format_content(_1)) }
       end
     end
 
