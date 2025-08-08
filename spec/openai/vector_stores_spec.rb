c2c750cbbc3bea1bf4bea4d2fe47adc396ec5f04
0x1eef <0x1eef@protonmail.com>
2025-08-08 18:44:18 +0000

    Add tests

---
+++ spec/openai/vector_stores_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "setup"
+
+RSpec.describe "LLM::OpenAI::VectorStores" do
+  let(:key) { ENV["OPENAI_SECRET"] || "TOKEN" }
+  let(:provider) { LLM.openai(key:) }
+
+  context "when given a successful create operation",
+          vcr: {cassette_name: "openai/vector_stores/successful_create"} do
+    subject(:store) { provider.vector_stores.create(name: "test store") }
+    after { provider.vector_stores.delete(vector: store) }
+
+    it "is successful" do
+      is_expected.to be_instance_of(LLM::Response)
+    end
+
+    it "includes created status" do
+      is_expected.to have_attributes(
+        "id" => instance_of(String),
+        "object" => "vector_store"
+      )
+    end
+  end
+
+  context "when given a successful get operation",
+          vcr: {cassette_name: "openai/vector_stores/successful_get"} do
+    let(:store) { provider.vector_stores.create(name: "test store") }
+    subject { provider.vector_stores.get(vector: store) }
+    after { provider.vector_stores.delete(vector: store) }
+
+    it "is successful" do
+      is_expected.to be_instance_of(LLM::Response)
+    end
+
+    it "includes the store" do
+      is_expected.to have_attributes(
+        "id" => store.id,
+        "name" => store.name
+      )
+    end
+  end
+
+  context "when given a successful delete operation",
+          vcr: {cassette_name: "openai/vector_stores/successful_delete"} do
+    let(:store) { provider.vector_stores.create(name: "test store") }
+    subject { provider.vector_stores.delete(vector: store) }
+
+    it "is successful" do
+      is_expected.to be_instance_of(LLM::Response)
+    end
+
+    it "includes deleted status" do
+      is_expected.to have_attributes(
+        "deleted" => true,
+      )
+    end
+  end
+end
