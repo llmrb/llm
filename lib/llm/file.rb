c650923df50a18bb2a514239bb1e49ea54aa5990
0x1eef <0x1eef@protonmail.com>
2025-08-10 23:44:52 +0000

    docs: update LLM::File

--- lib/llm/file.rb
+++ lib/llm/file.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 ##
-# The {LLM::File LLM::File} class represents a local file. It can
-# be used as a prompt with certain providers (eg: Ollama, Gemini),
-# and as an input with certain methods
+# {LLM::File LLM::File} represents a local file. It can be used
+# as a prompt with certain providers (eg: Ollama, Gemini),
+# and as an input with certain methods. It is usually not necessary
+# to create an instance of LLM::File directly. 
 class LLM::File
   ##
   # @return [String]
