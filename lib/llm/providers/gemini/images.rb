0458d914033628bc07636cb3e32e3fc9ac6f3299
0x1eef <0x1eef@protonmail.com>
2025-08-30 00:07:47 +0000

    gemini: separate create+edit prompts

--- lib/llm/providers/gemini/images.rb
+++ lib/llm/providers/gemini/images.rb
@@ -44,7 +44,7 @@ class LLM::Gemini
     def create(prompt:, model: "gemini-2.0-flash-exp-image-generation", **params)
       req  = Net::HTTP::Post.new("/v1beta/models/#{model}:generateContent?key=#{key}", headers)
       body = JSON.dump({
-        contents: [{parts: [{text: system_prompt}, {text: prompt}]}],
+        contents: [{parts: [{text: create_prompt}, {text: prompt}]}],
         generationConfig: {responseModalities: ["TEXT", "IMAGE"]}
       }.merge!(params))
       req.body = body
@@ -69,7 +69,7 @@ class LLM::Gemini
       req   = Net::HTTP::Post.new("/v1beta/models/#{model}:generateContent?key=#{key}", headers)
       image = LLM.File(image)
       body  = JSON.dump({
-        contents: [{parts: [{text: system_prompt}, {text: prompt}, format.format_content(image)]}],
+        contents: [{parts: [{text: edit_prompt}, {text: prompt}, format.format_content(image)]}],
         generationConfig: {responseModalities: ["TEXT", "IMAGE"]}
       }.merge!(params)).b
       set_body_stream(req, StringIO.new(body))
@@ -94,15 +94,27 @@ class LLM::Gemini
       @provider.instance_variable_get(:@key)
     end
 
-    def system_prompt
+    def create_prompt
       <<~PROMPT
         ## Context
-        Your task is to generate one or more image(s).
-        The user will provide you with text, and/or image(s).
+        Your task is to generate one or more image(s) based on the user's instructions.
+        The user will provide you with text only.
 
         ## Instructions
-        1. The model *MUST* generate image(s) based on the input(s).
-        2. The model *MUST* generate image(s).
+        1. The model *MUST* generate image(s) based on the user text alone.
+        2. The model *MUST NOT* generate anything else.
+      PROMPT
+    end
+
+    def edit_prompt
+      <<~PROMPT
+        ## Context
+        Your task is to edit the provided image based on the user's instructions.
+        The user will provide you with both text and an image.
+
+        ## Instructions
+        1. The model *MUST* edit the provided image based on the user's instructions
+        2. The model *MUST NOT* generate a new image.
         3. The model *MUST NOT* generate anything else.
       PROMPT
     end
